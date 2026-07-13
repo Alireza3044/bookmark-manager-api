5e8feb5b181ab1f282119662d0aaabe62b63e77e
Alireza Mohammadi <alireza1384928@gmail.com>
2026-07-13 08:45:16 +0330

    Remove frontend & minor improvements

--- bookmark_manager_api/settings.py
+++ bookmark_manager_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y8$(jwy(cb6_5$i!=&d_l($v!3wr0cw1c^7wsee@84d%5j^f@p'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
-    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'account_app',
